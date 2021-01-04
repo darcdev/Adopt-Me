bed1456b27054790d18bbacd4c2bc3ef6eaae79a
Diego Andres Cabrera Rojas <diegoandresrojas2000@gmail.com>
2021-01-04 08:45:29 -0500

    use context to theme color button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { Link, Router } from "@reach/router";
 import SearchParams from "./components/SearchParams";
 import Details from "./components/Details";
+import ThemeContext from "./context/ThemeContext";
+
 const App = () => {
+  const themeHook = useState("darkblue");
   return (
     <React.StrictMode>
-      <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
 
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
-      </div>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
     </React.StrictMode>
   );
 };
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,6 +1,9 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "../context/ThemeContext";
+
 class Details extends React.Component {
   state = { loading: true };
 
@@ -31,11 +34,21 @@ class Details extends React.Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+            )}
+          </ThemeContext.Consumer>
           <p>{description}</p>
         </div>
       </div>
     );
   }
 }
-export default Details;
+export default function DetailsWithErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
