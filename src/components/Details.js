c375ce361bf51e20da0ba10195ee719c1f145ee2
Diego Andres Cabrera Rojas <diegoandresrojas2000@gmail.com>
2021-01-03 21:35:20 -0500

    add detail render & babel configuration

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,7 +1,39 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
+class Details extends React.Component {
+  state = { loading: true };
 
-const Details = () => {
-  return "";
-};
+  async componentDidMount() {
+    let { animal } = await pet.animal(this.props.id);
 
+    this.setState({
+      name: animal.name,
+      animal: animal.type,
+      location: `${animal.contact.address.city} , ${animal.contact.address.state}`,
+      description: animal.description,
+      media: animal.photos,
+      breed: animal.breeds.primary,
+      loading: false,
+    });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
+
+    const { animal, breed, location, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 export default Details;
